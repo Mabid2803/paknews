01b383997b0254e953e0a773acf8463e7c447f49
Muhammad Abid <mabid2803@gmail.com>
2023-06-14 00:44:47 +0500

    paknews index page setup

--- public/index.php
+++ public/index.php
@@ -1,1 +1,261 @@
-hello world
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <title>Pak News</title>
+    <style>
+        body {
+            min-height: 75rem;
+            padding-top: 4.5rem;
+        }
+        .navbar
+        {
+            padding: 0;
+            background-color: rgba(246,249,250,.95);
+        }
+        .nav-link
+        {
+            padding: 1rem;
+        }
+        .navbar li:hover
+        {
+            background-color: #EFF4F5;
+        }
+        .bg
+        {
+            cursor: pointer;
+            background-position: center;
+            background-image: url("img/Suzuki-GSX-125-right-1-scaled-e1659112262119.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+            width: 670px;
+            height: 400px;
+            margin: 5px;
+        }
+        a
+        {
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm fixed-top navbar-light">
+    <div class="container">
+        <div style="background-color: #249991; margin-right: 30px" class="brand-logo">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <img style="margin: 15px 10px 15px 10px" src="img/pp_site_logo.png">
+        </div>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px; font-size: 1.2rem;">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Tech and Telecom</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Business</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">CarBase</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Sports</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Education</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Pakistan
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                        <li><a class="dropdown-item" href="#">ProProperty</a></li>
+                        <li><a class="dropdown-item" href="#">Perspective</a></li>
+                        <li><a class="dropdown-item" href="#">Aviation</a></li>
+                        <li><a class="dropdown-item" href="#">Social</a></li>
+                        <li><a class="dropdown-item" href="#">Career</a></li>
+                        <li><a class="dropdown-item" href="#">Videos</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Health</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <button class="btn" type="submit" style="background-color: #EFF4F5; padding: 1rem"><i style="font-size: 1.3rem" class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
+    </div>
+</nav>
+<main>
+    <div class="container mt-5" style="display: flex">
+        <a href="">
+            <div class="card bg">
+                <div class="card-body">
+                    <p style="text-align: center; font-size: 1.5rem; color: white; padding-top: 330px; font-weight: bold">Suzuki Shuts Down Bike Production Plant Again</p>
+                </div>
+            </div>
+        </a>
+        <div style="display: block">
+            <a href="">
+                <div class="card bg" style="width: 300px; height: 198px">
+                    <div class="card-body">
+                        <p style="text-align: left; font-size: 0.9rem; color: white; padding-top: 130px; font-weight: bold">Suzuki Shuts Down Bike Production Plant Again</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card bg" style="width: 300px; height: 198px">
+                    <div class="card-body">
+                        <p style="text-align: left; font-size: 0.9rem; color: white; padding-top: 130px; font-weight: bold">Suzuki Shuts Down Bike Production Plant Again</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div style="display: block">
+            <a href="">
+                <div class="card bg" style="width: 300px; height: 198px">
+                    <div class="card-body">
+                        <p style="text-align: left; font-size: 0.9rem; color: white; padding-top: 130px; font-weight: bold">Suzuki Shuts Down Bike Production Plant Again</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card bg" style="width: 300px; height: 198px">
+                    <div class="card-body">
+                        <p style="text-align: left; font-size: 0.9rem; color: white; padding-top: 130px; font-weight: bold">Suzuki Shuts Down Bike Production Plant Again</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="container mt-3 mb-5">
+        <h3 style="margin: 5px; border-left: 10px solid #249991; padding: 10px 15px; font-size: 1.3rem; color: #249991">LATEST NEWS</h3>
+        <div style="display: flex; margin-bottom: 30px">
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div style="display: flex; margin-bottom: 30px">
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div style="display: flex; margin-bottom: 30px">
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card" style="width: 19.5rem; border: none; margin-right: 15px">
+                    <img src="img/Gold-Price-Pakistan.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0">
+                        <p style="color: #249991; margin: 10px 0 10px 0; font-weight: bold">10:06 pm | Jun 13, 2023</p>
+                        <p class="card-text" style="font-weight: bold; font-size: 1.2rem; color: black">Price of Gold in Pakistan Sees Massive Decrease</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <button style="margin-left: 540px; margin-top: 50px; background-color: #1FB6AC; padding: 10px 50px; color: white; border: none; font-size: 1.2rem">SHOW MORE +</button>
+    </div>
+    <div style="width: 100%; margin-bottom: 20px; background-color: #1C1C1C">
+        <div class="container" style="padding: 30px 0">
+            <h3 style="border-left: 10px solid #3498DB; padding: 10px 15px; font-size: 1.3rem; color: #3498DB">TECH AND TELECOM</h3>
+        </div>
+    </div>
+</main>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</html>
